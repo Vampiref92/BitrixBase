fd2d6b7da101e2487afacbae971792a533a86874
frolov <sergeyfolov@inetsys.ru>
2018-09-10 14:53:40 +0300

    добавление стокового init.php

---
+++ src/additionalFiles/local/php_interface/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+define("LOG_FOLDER", "/local/log");
+define("LOG_FILENAME", $_SERVER["DOCUMENT_ROOT"]."/".LOG_FOLDER."main.log");
+define( "PATH_TO_404", "/404.php" );
+define( "PATH_TO_VENDOR_AUTOLOAD", "/local/vendor/autoload.php" );
