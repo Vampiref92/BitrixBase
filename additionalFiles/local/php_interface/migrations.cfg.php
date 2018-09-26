2a9de8d014edfa3b2de3013312a3480f6dee4601
frolov <sergeyfolov@inetsys.ru>
2018-09-26 13:25:04 +0300

    fix

--- additionalFiles/local/php_interface/migrations.cfg.php
+++ additionalFiles/local/php_interface/migrations.cfg.php
@@ -2,6 +2,6 @@
 
 return [
     'title'                  => 'PetrolTrade',
-    'migration_extend_class' => '\Vf92\Migration\SprintMigrationBase',
+    'migration_extend_class' => '\Vf92\BitrixUtils\Migration\SprintMigrationBase',
     'stop_on_errors'         => true,
 ];
