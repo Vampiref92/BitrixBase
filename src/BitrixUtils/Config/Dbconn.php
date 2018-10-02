66dd100fa7ce935fbb2442c7864e3dfe4853c370
frolov <sergeyfolov@inetsys.ru>
2018-10-02 17:34:00 +0300

    рефакторинг, расширение ключей, добавление ключа с кешовыми данными, реализован метод get

--- src/BitrixUtils/Config/Dbconn.php
+++ src/BitrixUtils/Config/Dbconn.php
@@ -11,12 +11,76 @@ namespace Vf92\BitrixUtils\Config;
 class Dbconn
 {
     /**
-     * @return bool|mixed|string
+     * @param $list
+     */
+    public static function save($list) {
+        $content = '';
+        if (!empty($list['db'])) {
+            $content .= '//Db' . PHP_EOL;
+            $content .= static::getStringDbContent($list['db']);
+        }
+
+        if (!empty($list['define']['db'])) {
+            $content .= static::getStringDefineContent($list['define']['db']);
+        }
+
+        if (!empty($list['define']['cached'])) {
+            $content .= '//Cached' . PHP_EOL;
+            $content .= static::getStringDefineContent($list['define']['cached']);
+        }
+
+        if (!empty($list['define']['permissions'])) {
+            $content .= '//Permissions' . PHP_EOL;
+            $content .= static::getStringDefineContent($list['define']['permissions']);
+        }
+
+        if (!empty($list['umask'])) {
+            $content .= '@umask(' . $list['umask'] . ')';
+            $content .= PHP_EOL;
+        }
+
+        if (!empty($list['define']['cache'])) {
+            $content .= '//Cache' . PHP_EOL;
+            $content .= static::getStringDefineContent($list['define']['cache']);
+        }
+
+        if (!empty($list['define']['project'])) {
+            $content .= '//Project' . PHP_EOL;
+            $content .= static::getStringDefineContent($list['define']['project']);
+        }
+
+        if (!empty($list['define']['custom'])) {
+            $content .= '//Custom' . PHP_EOL;
+            $content .= static::getStringDefineContent($list['define']['custom']);
+        }
+
+        if (!empty($list['custom'])) {
+            $content .= '//Unresolved values' . PHP_EOL;
+            $content .= $list['custom'];
+            $content .= PHP_EOL;
+        }
+
+        if (!empty($content)) {
+            $content = '<?php ' . PHP_EOL . $content;
+            file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/bitrix/php_interface/dbconn.php', $content);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public static function get()
+    {
+        return static::parse();
+    }
+
+    /**
+     * @return string
      */
     protected static function getFileContent()
     {
-        $file = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/bitrix/php_interface/dbconn.php');
-        $file = str_replace(['<?php', '<?', '?>'], '', $file);
+        $file = (string)file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/bitrix/php_interface/dbconn.php');
+        $file = (string)str_replace(['<?php', '<?', '?>'], '', $file);
         return $file;
     }
 
@@ -26,6 +90,9 @@ class Dbconn
     protected static function parse()
     {
         $file = static::getFileContent();
+        if(empty($file)){
+            return [];
+        }
 
         $defineList = [];
         $dbList = [];
@@ -111,6 +178,9 @@ class Dbconn
             if (preg_match('/(^DB)|(^MYSQL)|(.*_DB)|(.*_MYSQL)|(.*_CONNECT)/i', $key) !== false) {
                 $result['db'][$key] = $val;
             } elseif (preg_match('/^CACHED_/i', $key) !== false) {
+                $result['cached'][$key] = $val;
+            } elseif (preg_match('/(^BX_CACHE_TYPE$)|(^BX_CACHE_SID$)|(^BX_MEMCACHE_HOST$)|(^BX_MEMCACHE_PORT$)/i',
+                    $key) !== false) {
                 $result['cache'][$key] = $val;
             } elseif (preg_match('/_PERMISSIONS$/i', $key) !== false) {
                 $result['permissions'][$key] = $val;
@@ -122,42 +192,6 @@ class Dbconn
             }
         }
         return $result;
-    }
-
-    /**
-     * @param $list
-     */
-    public
-    static function save(
-        $list
-    ) {
-        $content = '<?php ';
-
-        $content .= '//Db' . PHP_EOL;
-        $content .= static::getStringDbContent($list['db']);
-
-        $content .= static::getStringDefineContent($list['define']['db']);
-
-        $content .= '//Cache' . PHP_EOL;
-        $content .= static::getStringDefineContent($list['define']['cache']);
-
-        $content .= '//Permissions' . PHP_EOL;
-        $content .= static::getStringDefineContent($list['define']['permissions']);
-
-        $content .= '@umask(' . $list['umask'] . ')';
-        $content .= PHP_EOL;
-
-        $content .= '//Project' . PHP_EOL;
-        $content .= static::getStringDefineContent($list['define']['project']);
-
-        $content .= '//Custom' . PHP_EOL;
-        $content .= static::getStringDefineContent($list['define']['custom']);
-
-        $content .= '//Unresolved values' . PHP_EOL;
-        $content .= $list['custom'];
-        $content .= PHP_EOL;
-
-        file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/bitrix/php_interface/dbconn.php', $content);
     }
 
     /**
@@ -209,14 +243,5 @@ class Dbconn
         $content .= PHP_EOL;
 
         return $content;
-    }
-
-    /**
-     * @return array
-     */
-    public
-    static function get()
-    {
-        return [];
     }
 }
