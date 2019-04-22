01ea901126748bf0e29518032c72073bcd6a10c4
Dmitry <d.prohorov@inetsys.ru>
2019-04-22 18:20:38 +0300

    Check iblock for method

--- src/BitrixUtils/Iblock/ElementHelper.php
+++ src/BitrixUtils/Iblock/ElementHelper.php
@@ -5,10 +5,13 @@ namespace Vf92\BitrixUtils\Iblock;
 
 
 use Bitrix\Iblock\ElementTable;
+use Bitrix\Iblock\IblockTable;
 use Bitrix\Main\ArgumentException;
 use Bitrix\Main\ObjectPropertyException;
 use Bitrix\Main\SystemException;
+use mysql_xdevapi\Result;
 use Vf92\BitrixUtils\Config\Version;
+use Vf92\BitrixUtils\Iblock\Exception\IblockNotFoundException;
 
 /**
  * Class ElementHelper
@@ -43,5 +46,21 @@ class ElementHelper
             return null;
         }
         return $id > 0 ? $id : null;
+    }
+    public static function copy($elementId,$sectionId = false,$iblockId = false){
+        $result = new Result();
+        if((int)$iblockId>0){
+            $queryResult = IblockTable::query()
+                ->setSelect(['ID'])
+                ->where('ID',$iblockId)
+                ->exec();
+            if(!$queryResult->fetch()){
+                throw new IblockNotFoundException();
+            }
+        }
+        elseif ($iblockId!==false){
+            throw new ArgumentException('Идентификатор инфоблока не является числом, большим 0','iblockId');
+        }
+        return $result;
     }
 }
